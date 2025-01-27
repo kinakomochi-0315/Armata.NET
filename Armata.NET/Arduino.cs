b53163fbb2fe8b89b1ce4d7610677dd82e198c69
kinakomochi0315 <kinakomochi.riku@gmail.com>
2025-01-27 19:44:18 +0900

    ピン番号をbyte型からint型に変更（めんどくさかったので）

--- Armata.NET/Arduino.cs
+++ Armata.NET/Arduino.cs
@@ -13,6 +13,7 @@ public enum PinMode
 public class Arduino(string portName, int baudRate = 57600)
 {
     private const int DIGITAL_PORT_COUNT = 8;
+    private const int DIGITAL_PIN_COUNT = DIGITAL_PORT_COUNT * 8;
     private const int ANALOG_PIN_COUNT = 16;
 
     private const int SYSEX_BEGIN = 0xF0;
@@ -26,36 +27,82 @@ public class Arduino(string portName, int baudRate = 57600)
 
     public bool IsConnected => _serialPort.IsOpen;
 
-    private static byte GetPort(byte pin)
+    /// <summary>
+    /// Firmataプロトコルにおいてデジタルピンの状態を取得・設定するために使用されるポートという単位に
+    /// ピン番号を変換します。ポートは8ビットのデジタルピンをグループ化したものです。
+    /// </summary>
+    /// <param name="pin">変換するピン番号</param>
+    /// <returns>ピン番号が所属するポート番号</returns>
+    private static int GetPort(int pin)
     {
-        return (byte)(pin / 8);
+        return pin / 8;
     }
 
+    /// <summary>
+    /// Firmataプロトコルの7ビットの2つのバイトを結合して1つの整数値に変換します。
+    /// </summary>
+    /// <param name="low">前ビット</param>
+    /// <param name="high">後ビット</param>
+    /// <returns></returns>
+    private static int ConcatBytes(int low, int high)
+    {
+        return low | high << 7;
+    }
+
+    /// <summary>
+    /// 整数値を7ビットの2つのバイトに分割します。
+    /// </summary>
+    /// <param name="value">分割するビット</param>
+    /// <returns>変換後のバイト列(low, high)</returns>
+    private static byte[] SplitBytes(int value)
+    {
+        return [(byte)(value % 127), (byte)(value >> 7)];
+    }
+
+    /// <summary>
+    /// Arduinoとのシリアル通信を確立します。
+    /// </summary>
     public void Connect()
     {
         _serialPort.Open();
         Task.Run(SerialReceivingTask);
     }
 
+    /// <summary>
+    /// Arduinoとのシリアル通信を切断します。
+    /// </summary>
     public void Disconnect()
     {
-        CheckSerialPortIsValid();
+        if (_serialPort is not { IsOpen: true })
+        {
+            throw new InvalidOperationException("Not connected to Arduino, please call Connect() method to connect.");
+        }
 
         _serialPort.Close();
         _serialPort.Dispose();
     }
 
-    public void PinMode(byte pin, PinMode mode)
+    /// <summary>
+    /// ArduinoのGPIOピンのモードを設定します。
+    /// </summary>
+    /// <param name="pin">設定を変更するピンの番号</param>
+    /// <param name="mode">ピンのモード</param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public void PinMode(int pin, PinMode mode)
     {
-        if (pin >= DIGITAL_PORT_COUNT * 8)
+        if (pin is not (>= 0 and < DIGITAL_PIN_COUNT))
         {
+            // ピン番号がFirmataプロトコルの範囲外のとき
             throw new ArgumentOutOfRangeException(nameof(pin));
         }
 
-        CheckSerialPortIsValid();
+        if (_serialPort is not { IsOpen: true })
+        {
+            throw new InvalidOperationException("Not connected to Arduino, please call Connect() method to connect.");
+        }
 
         // ピンのモードを設定
-        var modeReq = new byte[] { 0xF4, pin, (byte)mode };
+        var modeReq = new byte[] { 0xF4, (byte)pin, (byte)mode };
         _serialPort.Write(modeReq, 0, modeReq.Length);
 
         // そのピンの状態が変更されたときに通知するようリクエスト
@@ -63,101 +110,116 @@ public class Arduino(string portName, int baudRate = 57600)
         {
             case NET.PinMode.Input:
             {
-                var digitalNotifyReq = new byte[] { (byte)(0xD0 + GetPort(pin)), 1 };
+                byte[] digitalNotifyReq = [(byte)(0xD0 + GetPort(pin)), 1];
                 _serialPort.Write(digitalNotifyReq, 0, digitalNotifyReq.Length);
                 break;
             }
             case NET.PinMode.Analog:
             {
-                var analogNotifyReq = new byte[] { (byte)(0xC0 + pin), 1 };
+                byte[] analogNotifyReq = [(byte)(0xC0 + pin), 1];
                 _serialPort.Write(analogNotifyReq, 0, analogNotifyReq.Length);
                 break;
             }
         }
     }
 
-    public void DigitalWrite(byte pin, PinState state)
+    /// <summary>
+    /// デジタルピンの状態を設定します。
+    /// </summary>
+    /// <param name="pin">状態を設定するピンの番号</param>
+    /// <param name="state">設定する状態</param>
+    /// <exception cref="ArgumentOutOfRangeException">Firmata範囲(0~127)外のピン番号を指定したとき</exception>
+    /// <exception cref="InvalidOperationException">connect()を呼び出してArduinoに接続していないとき</exception>
+    public void DigitalWrite(int pin, PinState state)
     {
-        if (pin >= DIGITAL_PORT_COUNT * 8)
+        if (pin is not (>= 0 and < DIGITAL_PIN_COUNT))
         {
+            // ピン番号がFirmataプロトコルの範囲外のとき
             throw new ArgumentOutOfRangeException(nameof(pin));
         }
 
-        CheckSerialPortIsValid();
+        if (_serialPort is not { IsOpen: true })
+        {
+            throw new InvalidOperationException("Not connected to Arduino, please call Connect() method to connect.");
+        }
 
-        var bytes = new byte[] { DIGITAL_MESSAGE, pin, (byte)state };
-        _serialPort.Write(bytes, 0, bytes.Length);
+        byte[] digitalWriteReq = [DIGITAL_MESSAGE, (byte)pin, (byte)state];
+        _serialPort.Write(digitalWriteReq, 0, digitalWriteReq.Length);
     }
 
-    public PinState DigitalRead(byte pin)
+    /// <summary>
+    /// デジタルピンの状態を取得します。
+    /// </summary>
+    /// <param name="pin">状態を取得するピンの番号</param>
+    /// <returns>デジタルピンの状態</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Firmata範囲(0~127) 外のピン番号を指定したとき</exception>
+    /// <exception cref="InvalidOperationException">connect() を呼び出してArduinoに接続していないとき</exception>
+    public PinState DigitalRead(int pin)
     {
-        if (pin >= DIGITAL_PORT_COUNT * 8)
+        if (pin is not (>= 0 and < DIGITAL_PIN_COUNT))
         {
+            // ピン番号がFirmataプロトコルの範囲外のとき
             throw new ArgumentOutOfRangeException(nameof(pin));
         }
 
-        CheckSerialPortIsValid();
+        if (_serialPort is not { IsOpen: true })
+        {
+            throw new InvalidOperationException("Not connected to Arduino, please call Connect() method to connect.");
+        }
 
         return _digitalStates[pin];
     }
 
-    public int AnalogRead(byte pin)
+    public int AnalogRead(int analogPin)
     {
-        if (pin >= ANALOG_PIN_COUNT)
+        if (analogPin is not (>= 0 and < DIGITAL_PIN_COUNT))
         {
-            throw new ArgumentOutOfRangeException(nameof(pin));
+            // ピン番号がFirmataプロトコルの範囲外のとき
+            throw new ArgumentOutOfRangeException(nameof(analogPin));
         }
 
-        CheckSerialPortIsValid();
+        if (_serialPort is not { IsOpen: true })
+        {
+            throw new InvalidOperationException("Not connected to Arduino, please call Connect() method to connect.");
+        }
 
-        return _analogStates[pin];
+        return _analogStates[analogPin];
     }
 
-    public void AnalogWrite(byte pin, int value)
+    public void AnalogWrite(int pin, int value)
     {
-        if (pin >= DIGITAL_PORT_COUNT * 8)
+        if (pin is not (>= 0 and < DIGITAL_PIN_COUNT))
         {
+            // ピン番号がFirmataプロトコルの範囲外のとき
             throw new ArgumentOutOfRangeException(nameof(pin));
         }
 
         if (value < 0) value = 0;
 
-        CheckSerialPortIsValid();
-
-        var bytes = new[] { (byte)(ANALOG_MESSAGE + pin), (byte)(value % 127), (byte)(value >> 7) };
-        _serialPort.Write(bytes, 0, bytes.Length);
-    }
-
-    private void CheckSerialPortIsValid()
-    {
         if (_serialPort is not { IsOpen: true })
         {
             throw new InvalidOperationException("Not connected to Arduino, please call Connect() method to connect.");
         }
+
+        byte[] bytes = [(byte)(ANALOG_MESSAGE + pin), ..SplitBytes(value)];
+        _serialPort.Write(bytes, 0, bytes.Length);
     }
 
     private void ReadDigitalState(int port)
     {
-        var pinStates = ReadCommand(2).ToArray();
+        var bytes = ReadCommand(2).ToArray();
+        var pinStates = ConcatBytes(bytes[0], bytes[1]);
 
-        // 0~7ビット目を読み取る
-        for (var i = 0; i < 7; i++)
+        for (var i = 0; i < 8; i++)
         {
-            var idx = port * 8 + i;
-            var value = pinStates[0] >> i & 1;
-            _digitalStates[idx] = (PinState)value;
+            _digitalStates[port * 8 + i] = (PinState)((pinStates >> i) & 1);
         }
-
-        // 8ビット目を読み取る
-        _digitalStates[(port + 1) * 8 - 1] = (PinState)(pinStates[1] & 1);
     }
 
     private void ReadAnalogValue(int pin)
     {
-        var state = ReadCommand(2).ToArray();
-        var value = state[1] << 7 | state[0];
-
-        _analogStates[pin] = value;
+        var bytes = ReadCommand(2).ToArray();
+        _analogStates[pin] = ConcatBytes(bytes[0], bytes[1]);
     }
 
     private IEnumerable<int> ReadSysexArgs()
